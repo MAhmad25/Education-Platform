fa4ddb918468db4fdb1255737af889eabe01f923
MAhmad25 <userarenotproduct@gmail.com>
2026-01-20 00:51:58 +0500

    Starting implementing the UI

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+      return (
+            <main className="bg-bg h-dvh w-dvw text-txt">
+                  <Hero />
+            </main>
+      );
+};
+export default App;
